cc102033456e9d6c25b9d5066a4d878eed098362
AttiqaNazir <atiqanazir15@gmail.com>
2024-03-18 22:20:34 +0100

    visualize train schedule for one route and more than one trip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import './App.css';
-import React, { useState, useEffect } from "react";
-import Papa from 'papaparse';
+import React, { useState, useEffect } from 'react';
 import Schedule from './components/Schedule';
-import { parseStopTimesDataForRoute, parseStopsData } from './utils/gtfsParser';
+import { parseStopsData } from './utils/gtfsParser';
 
 function App() {
-  const [routes, setRoutes] = useState([]);
-  const [trips, setTrips] = useState([]);
-
-  const [stopTimes, setStopTimes] = useState([]); 
   const [stopsData, setStopsData] = useState({});
-
-  const [startTime, setStartTime] = useState('08:00:00'); 
-  const [endTime, setEndTime] = useState('10:00:00');
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const stopsDataResponse = await parseStopsData();
         setStopsData(stopsDataResponse);
-
       } catch (error) {
         console.error('Error fetching or parsing GTFS data:', error);
       }
@@ -29,9 +20,7 @@ function App() {
   }, []);
 
   return (
-     <div className="app">
-      {Object.keys(stopsData).length && <Schedule data={stopsData} />}
-    </div>
+    <div className="app">{Object.keys(stopsData).length && <Schedule data={stopsData} />}</div>
   );
 }
 
--- src/utils/gtfsParser.js
+++ src/utils/gtfsParser.js
@@ -1,95 +1,98 @@
 export const parseStopsData = async () => {
   try {
-    // Read the stops.txt file
-    const tripResponse = await fetch("./TrainSchedule/trips.txt");
+    const tripResponse = await fetch('./TrainSchedule/trips.txt');
     const tripText = await tripResponse.text();
 
-    const stopTimesResponse = await fetch("./TrainSchedule/stop_times.txt");
+    const stopTimesResponse = await fetch('./TrainSchedule/stop_times.txt');
     const stopTimesText = await stopTimesResponse.text();
 
-    const stopResponse = await fetch("./TrainSchedule/stops.txt");
+    const stopResponse = await fetch('./TrainSchedule/stops.txt');
     const stopsText = await stopResponse.text();
 
-    // Split the data by lines
-    const tripDataSplit = tripText.split("\n");
+    const tripDataSplit = tripText.split('\n');
     const tripData = [];
     tripDataSplit.forEach((line) => {
       const trimmed = line.trim();
       if (trimmed) {
-        const [route_id, , trip_id] = trimmed.split(",");
+        const [route_id, , trip_id] = trimmed.split(',');
 
         if (!isNaN(parseInt(route_id, 10))) {
           tripData.push({
             routeId: route_id.trim(),
-            tripId: trip_id.trim(),
+            tripId: trip_id.trim()
           });
         }
       }
     });
-    const tripDataFiltered = tripData.filter((data) => data.routeId === '1' && data.tripId === '1')
+
+    const tripIds = tripData.slice(0, 2).map((data) => data.tripId);
+
+    const tripsData = {};
+
+    tripIds.forEach((tripId) => {
+      tripsData[`trip_id_${tripId}`] = { stops: [] };
+    });
 
     const stopTimesSplit = stopTimesText.split('\n');
     const stopsTimesData = [];
     stopTimesSplit.forEach((line) => {
       const trimmed = line.trim();
       if (trimmed) {
-        const [trip_id, arrival_time, departure_time, stop_id] = trimmed.split(",");
+        const [trip_id, arrival_time, departure_time, stop_id] = trimmed.split(',');
 
         if (!isNaN(parseInt(trip_id, 10))) {
-
           stopsTimesData.push({
             tripId: trip_id.trim(),
             arrivalTime: arrival_time.trim(),
             departureTime: departure_time.trim(),
-            stopId: stop_id.trim(),
+            stopId: stop_id.trim()
           });
         }
       }
     });
-    
-    const stopTimesDataFiltered = stopsTimesData.filter((data) => data.tripId === '1');
+
+    const stopTimesDataFiltered = stopsTimesData.filter((data) => tripIds.includes(data.tripId));
 
     const stopsSplit = stopsText.split('\n');
     const stopsData = [];
     stopsSplit.forEach((line) => {
       const trimmed = line.trim();
       if (trimmed) {
-        const [stop_id, stop_code, stop_name] = trimmed.split(",");
+        const [stop_id, stop_code, stop_name] = trimmed.split(',');
 
         if (stop_id !== 'stop_id') {
           stopsData.push({
             stopId: stop_id.trim(),
             stopCode: stop_code.trim(),
-            stopName: stop_name.trim(),
+            stopName: stop_name.trim()
           });
         }
       }
     });
 
-    const stops = []
+    const stops = [];
     stopTimesDataFiltered.forEach((stopTime) => {
-        stopsData.forEach((stop) => {
-            if (stop.stopId === stopTime.stopId) {
-                stops.push({
-                    ...stopTime,
-                    ...stop,
-                })
-            }
-        })
-    })
+      stopsData.forEach((stop) => {
+        if (stop.stopId === stopTime.stopId) {
+          stops.push({
+            ...stopTime,
+            ...stop
+          });
+        }
+      });
+    });
 
-    const locations = new Set()
+    const locations = new Set();
+
     stops.forEach((stop) => {
-        locations.add(stop.stopName);
+      locations.add(stop.stopName);
     });
 
     return {
-        stops,
-        locations: Array.from(locations),
+      stops,
+      locations: Array.from(locations)
     };
-
   } catch (error) {
-    console.error("Error parsing stops data:", error);
-    throw new Error(error.message ?? 'Something went wrong parsing the data')
+    throw new Error(error.message ?? 'Something went wrong parsing the data');
   }
 };
